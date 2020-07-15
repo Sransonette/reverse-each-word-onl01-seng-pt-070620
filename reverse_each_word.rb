70d8b9ac18b8f814d739fd8d1b5dce3eeaa9d273
Sean Ransonette <sransonette@gmail.com>
2020-07-15 01:38:44 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(sentence)
+  sentence_1 = []
+  sentence.split.each do |word|
+    sentence_1 << word.reverse
+  end
+  sentence_1.join(" ")
+end
+  
+def reverse_each_word(sentence)
+  sentence_1 = []
+  sentence.split.collect do |word|
+    sentence_1 << word.reverse
+  end
+  sentence_1.join(" ")
+end
