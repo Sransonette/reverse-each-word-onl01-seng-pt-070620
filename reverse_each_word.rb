690cabb34b189e5175d53606aa16aa29806e4f07
Sean Ransonette <sransonette@gmail.com>
2020-07-15 00:51:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,8 @@
 def reverse_each_word(sentence)
   sentence_1 = []
-  sentence.each do |words|
-    sentence.split
-    sentence.reverse
-    sentence_1 << "#{words}"
-  end
- sentence_1
+  sentence.split.each
+  sentence.split(",").reverse.join(",")
+  
 end
   
   
